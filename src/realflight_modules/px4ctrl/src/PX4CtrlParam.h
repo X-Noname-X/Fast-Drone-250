54603253f66461faa40ab8ef0b5f3f509fb3645f
X-Noname-X <xiao_yq317@163.com>
2025-11-04 20:44:53 +0800

    Update PX4CtrlParam.h

--- src/realflight_modules/px4ctrl/src/PX4CtrlParam.h
+++ src/realflight_modules/px4ctrl/src/PX4CtrlParam.h
@@ -6,30 +6,32 @@
 class Parameter_t
 {
 public:
+	//嵌入结构体
+	//控制增益
 	struct Gain
 	{
-		double Kp0, Kp1, Kp2;
-		double Kv0, Kv1, Kv2;
-		double Kvi0, Kvi1, Kvi2;
-		double Kvd0, Kvd1, Kvd2;
-		double KAngR, KAngP, KAngY;
+		double Kp0, Kp1, Kp2;  //位置比例(P) 对应x,y,z轴
+		double Kv0, Kv1, Kv2;  //速度比例(P)
+		double Kvi0, Kvi1, Kvi2;  //速度积分(I)
+		double Kvd0, Kvd1, Kvd2;  //速度微分(D)
+		double KAngR, KAngP, KAngY; //角度比例，对应 横滚、俯仰、偏航
 	};
-
+	// 旋翼阻力参数
 	struct RotorDrag
 	{
-		double x, y, z;
-		double k_thrust_horz;
+		double x, y, z; //阻力系数
+		double k_thrust_horz; //水平推力系数
 	};
-
+	//定义各传感器/控制消息的超时时间
 	struct MsgTimeout
 	{
-		double odom;
-		double rc;
-		double cmd;
-		double imu;
-		double bat;
+		double odom; //里程计
+		double rc;  //遥控器
+		double cmd; //控制命令
+		double imu; //陀螺仪
+		double bat; //电池状态
 	};
-
+	// 推力映射模型
 	struct ThrustMapping
 	{
 		bool print_val;
@@ -39,24 +41,24 @@ public:
 		bool accurate_thrust_model;
 		double hover_percentage;
 	};
-
+	//布尔值标识各通道是否需要反向
 	struct RCReverse
 	{
 		bool roll;
 		bool pitch;
 		bool yaw;
-		bool throttle;
+		bool throttle; //油门
 	};
-
+	//自动起降
 	struct AutoTakeoffLand
 	{
-		bool enable;
-		bool enable_auto_arm;
-		bool no_RC;
-		double height;
-		double speed;
+		bool enable; //使能
+		bool enable_auto_arm; //使能自动解锁
+		bool no_RC; //是否无需遥控器
+		double height; //目标高度
+		double speed; //起降速度
 	};
-
+	//用结构体定义变量
 	Gain gain;
 	RotorDrag rt_drag;
 	MsgTimeout msg_timeout;
